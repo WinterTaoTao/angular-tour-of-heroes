40f9f0a468191888b6715aa38e7e51d23a100a4a
Winter <xwhwt92@hotmail.com>
2017-11-08 17:03:48 +1000

    Display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,8 +13,13 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+  heroes = HEROES;
+  selectedHero: Hero;
   constructor() {
 
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
   ngOnInit() {
